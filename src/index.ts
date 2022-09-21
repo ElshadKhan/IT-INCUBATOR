5ae3abd5eaef640985b745d9373e8a85918a4580
Elshad <khanakhmedov.elshad@mail.ru>
2022-09-21 12:33:10 +0300

    first committttt

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from 'express'
 // import {router} from "./routes/IndexRoutes";
 import {blogRouter} from "./routes/blogRouter";
 import {postRouter} from "./routes/postRouter";
-import {config} from 'dotenv'
-config()
+// import {config} from 'dotenv'
+// config()
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 
 app.use(express.json());
 
