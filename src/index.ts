bf02e108a3ee703511c7efdeb26a270b2cab7487
Elshad <khanakhmedov.elshad@mail.ru>
2022-09-21 12:57:51 +0300

    make it better

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import express from 'express'
 // import {router} from "./routes/IndexRoutes";
 import {blogRouter} from "./routes/blogRouter";
 import {postRouter} from "./routes/postRouter";
+import  cors from 'cors'
 import {router} from "./routes/IndexRoutes";
-// import {config} from 'dotenv'
-// config()
+import {config} from "dotenv";
+config()
 
 const app = express()
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.json());
 app.delete("/testing/all-data", (req, res) => {
     return []
