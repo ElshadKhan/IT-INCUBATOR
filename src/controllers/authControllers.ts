b3afd79e3d92145169924250bdb9afd3829d5886
Elshad <khanakhmedov.elshad@mail.ru>
2022-10-13 23:05:48 +0300

    Big fix committttt

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -21,11 +21,8 @@ export const authControllers = {
     },
     async createUser(req: Request, res: Response) {
         const user = await authService.createUser(req.body.login, req.body.password, req.body.email)
-        if(user) {
-            res.status(204).send("Input data is accepted. Email with confirmation code will be send to passed email address")
-        } else {
-            res.status(404)
-        }
+        res.send("Input data is accepted. Email with confirmation code will be send to passed email address")
+
     },
     async confirmationEmail(req: Request, res: Response) {
         const user = await authService.confirmationEmail(req.body.code)
