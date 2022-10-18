a03f74aa39880e38209e3fc6a094f8063307abfa
Elshad <khanakhmedov.elshad@mail.ru>
2022-10-18 18:49:52 +0300

    Big fix committttt

--- src/repositories/queryRep/userQueryRepository.ts
+++ src/repositories/queryRep/userQueryRepository.ts
@@ -4,8 +4,8 @@ import {getPagesCounts, getSkipNumber} from "../../helpers/helpFunctions";
 
 export const userQueryRepository = {
     async findUsers({searchLoginTerm, searchEmailTerm, pageNumber, pageSize, sortBy, sortDirection}: QueryUserType): Promise<UsersBusinessType> {
-        const users = await usersCollection.find({$or: [{login: {$regex: searchLoginTerm, $options: "(?i)a(?-i)cme"}}, {email: {$regex: searchEmailTerm, $options: "(?i)a(?-i)cme"}}]}).sort(sortBy, sortDirection).skip(getSkipNumber(pageNumber,pageSize)).limit(pageSize).toArray()
-        const totalCountUsers = await usersCollection.find( {$or: [{login: {$regex: searchLoginTerm, $options: "(?i)a(?-i)cme"}}, {email: {$regex: searchEmailTerm, $options: "(?i)a(?-i)cme"}}]}).count()
+        const users = await usersCollection.find({$or: [{'accountData.userName': {$regex: searchLoginTerm, $options: "(?i)a(?-i)cme"}}, {'emailConfirmation.confirmationCode': {$regex: searchEmailTerm, $options: "(?i)a(?-i)cme"}}]}).sort(sortBy, sortDirection).skip(getSkipNumber(pageNumber,pageSize)).limit(pageSize).toArray()
+        const totalCountUsers = await usersCollection.find( {$or: [{'accountData.userName': {$regex: searchLoginTerm, $options: "(?i)a(?-i)cme"}}, {'emailConfirmation.confirmationCode': {$regex: searchEmailTerm, $options: "(?i)a(?-i)cme"}}]}).count()
         const userDto = {
             "pagesCount": getPagesCounts(totalCountUsers, pageSize),
             "page": pageNumber,
