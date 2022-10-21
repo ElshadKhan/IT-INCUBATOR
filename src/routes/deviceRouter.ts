6bbbdfda01bb06736a006f8e4c878507bb1f3de3
Elshad <khanakhmedov.elshad@mail.ru>
2022-10-21 22:28:27 +0300

    Big fix committttt

--- src/routes/deviceRouter.ts
+++ src/routes/deviceRouter.ts
@@ -1,10 +1,9 @@
 import {Router} from "express";
 import {refreshTokenMiddleware} from "../middleware/refreshTokenValidation";
 import {sessionsControllers} from "../controllers/deviceControllers";
-import {deviceIdValidations} from "../middleware/deviceMiddleware/deviceMiddlewares";
 
 export const sessionsRouter = Router({})
 
 sessionsRouter.get('/devices', refreshTokenMiddleware, sessionsControllers.getAllActiveSessions)
 sessionsRouter.delete('/devices', refreshTokenMiddleware, sessionsControllers.deleteSessionsByDeviceId)
-sessionsRouter.delete('/devices/:deviceId', refreshTokenMiddleware, deviceIdValidations, sessionsControllers.deleteAllSessionsExceptOne)
+sessionsRouter.delete('/devices/:deviceId', refreshTokenMiddleware, sessionsControllers.deleteAllSessionsExceptOne)
